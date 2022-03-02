cf8bce5cb600bf825dbf7d27e9742856fe897aab
KelvinNthiana <98016339+KelvinNthiana@users.noreply.github.com>
2022-03-02 21:35:42 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,35 @@
+
+/*
+   project:county library management system
+   Author: Kelvin Nthiana
+   Date:   March,2022
+   Compiler: GNU GCC
+   Language C99
+   License:MIT
+*/
+#include <studio.h>
+#include <studio.h>
+
+int main[]
+[
+   printf["\tCounty Library\n"];
+   printf["welcome Mr. Nthiana. \n"];
+   printf["what would you like to do?\n"];
+   printf["1. Add new user\n"];
+   printf["2. Add book information\n"];
+   printf["3. Display book information\n"];
+   printf["4. List books of given author\n"];
+   printf["5. List title of specified book\n"];
+   printf["6. List the count of books in the library\n"];
+   printf["selected action: "];
+   return 0;
+]
+
+
+
+
+
+
+
+
+
